442cdb6696bd47208f6763adf4a6c454c95e929b
Venugopal Ranganathan <95169245+V-Rang@users.noreply.github.com>
2023-03-25 14:40:48 -0500

    Update pi_parallel.c

--- pi_parallel.c
+++ pi_parallel.c
@@ -2,25 +2,33 @@
 #include<stdio.h>
 #include<time.h>
 
-static long num_steps = 100000;
+static long num_steps = 1000000000;
 double step;
+
+
 
 void main()
 {
     //Serial pi
+    clock_t start,end;
     int i;
     step = (1-0)/(double)num_steps;
     double x,pi,sum=0.0;
 
+    start = clock();
     for(i=0;i<num_steps;i++)
     {
         sum +=  4/(1 + ( i/(double)num_steps)*( i/(double)num_steps));
     }
     pi = step*sum;
-    printf("pi value = %f\n",pi);
+    end = clock();
+    // printf("pi value = %f\n",pi);
+    printf("pi value = %f and time taken = %f\n",pi,((float)end-(float)start)/CLOCKS_PER_SEC);
+
 
     //Method 1
     sum = 0.0;
+    start = clock();
     #pragma omp parallel num_threads(4) reduction(+:sum) shared(num_steps) private(i) default(none)
     {
         #pragma omp for
@@ -30,10 +38,13 @@ void main()
         }
     }
     pi = step*sum;
-    printf("pi value = %f\n",pi);
+    end = clock();
+    // printf("pi value = %f\n",pi);
+    printf("pi value = %f and time taken = %f\n",pi, ( (float)end-(float)start)/CLOCKS_PER_SEC);
 
     //Method 2
     sum = 0.0;
+    start = clock();
     #pragma omp parallel for num_threads(4) reduction(+:sum) shared(num_steps) private(i) default(none)
     for(i=0;i<num_steps;i++)
     {
@@ -41,7 +52,9 @@ void main()
     }
     
     pi = step*sum;
-    printf("pi value = %f\n",pi);
+    end = clock();
+    printf("pi value = %f and time taken = %f\n",pi, ( (float)end-(float)start)/CLOCKS_PER_SEC);
+    // printf("pi value = %f\n",pi);
 
 
 }
