1c77fc1e946a7515c98dd16b7c04ebf7194681da
V-Rang <venu1998@gmail.com>
2023-05-12 20:14:05 -0500

    for array of sizes 2^(int)

--- prefix_scan1.cpp
+++ prefix_scan1.cpp
@@ -28,13 +28,13 @@ int main()
 {
 
     const double minval  = 0.0;
-    const double maxval = 1.0; 
+    const double maxval = 100.0; 
     
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double>dist(minval,maxval);
 
-    int n = (int)8;
+    int n = (int)pow(2,10);
     double a[n],a_ser_incl[n],a2[n],a_ser_excl[n],a3[n];
     int i,j;
     //creating an array a: 1 2 3 4 5 6 7 8
@@ -42,18 +42,24 @@ int main()
     //a3 is same as a and is used for the exclusive scan case.
     for(i=0;i<n;i++)
     {
-        // a[i] = dist(gen);
-        a[i] = i+1;
+        a[i] = dist(gen);
+        // a[i] = i+1;
         a_ser_incl[i] = a[i];
         a3[i] = a[i];
     }
+
+    // for(i=0;i<n;i++)
+    // {
+    //     cout << a[i] << " ";
+    // }
+    // cout << endl;
     
-    printf("The array to be scanned is: ");
-    for(i=0;i<n;i++)
-    {
-        cout << a[i] << " ";
-    }
-    cout << endl;
+    // printf("The array to be scanned is: ");
+    // for(i=0;i<n;i++)
+    // {
+    //     cout << a[i] << " ";
+    // }
+    // cout << endl;
 
 
     
@@ -76,19 +82,19 @@ int main()
     }
 
 
-    printf("Inclsive scan performed serially: ");
-    for(i = 0;i<n;i++)
-    {
-        cout << a_ser_incl[i] << " ";
-    }
-    cout << endl;
+    // printf("Inclsive scan performed serially: ");
+    // for(i = 0;i<n;i++)
+    // {
+    //     cout << a_ser_incl[i] << " ";
+    // }
+    // cout << endl;
 
-    printf("Inclsive scan performed parallely using Hillis-Steele Algorithm: ");
-    for(i = 0;i<n;i++)
-    {
-        cout << a[i] << " ";
-    }
-    cout << endl;
+    // printf("Inclsive scan performed parallely using Hillis-Steele Algorithm: ");
+    // for(i = 0;i<n;i++)
+    // {
+    //     cout << a[i] << " ";
+    // }
+    // cout << endl;
 
 
     //exclusive scan using Blelloch algorithm - D=O(logn) W=O(n)
@@ -100,7 +106,8 @@ int main()
 
     for(i=1;i<=floor(log2(n));i++)
     {
-        for(j=pow(2,i)-1;j<n;j+= pow(2,i))
+        #pragma omp parallel for private(j) shared(i,a3,n) default(none)
+        for(j=pow(2,i)-1;j<n;j+= (int)pow(2,i))
         {
             a3[j] += a3[(int)(j-pow(2,i-1))];
         }
@@ -111,7 +118,8 @@ int main()
 
     for(i=floor(log2(n));i>=1;i--)
     {
-        for(j=pow(2,i)-1;j<n;j += pow(2,i))
+        #pragma omp parallel for private(j,temp) shared(i,a3,n) default(none) 
+        for(j=pow(2,i)-1;j<n;j += (int)pow(2,i))
         {
             temp = a3[(int)(j-pow(2,i-1))];
             a3[(int)(j-pow(2,i-1))] = a3[j];
@@ -119,64 +127,37 @@ int main()
         }
     }
 
-    printf("Exclusive scan performed serially: ");
-    for(i = 0;i<n;i++)
-    {
-        cout << a_ser_excl[i] << " ";
-    }
-    cout << endl;
-
-    printf("Exclusive scan performed parallely using Blelloch Algorithm: ");
-    for(i = 0;i<n;i++)
-    {
-        cout << a3[i] << " ";
-    }
-    cout << endl;
-
-
-
-
-
-    // a_ser[0] = 0;
-    // for(i=1;i<n;i++)
-    // {
-    //     a_ser[i] = a_ser[i-1] + a[i-1];
-    // }   
-
-
-    // for(i=1;i<=floor(log2(n));i++)
-    // {
-    //     #pragma omp parallel for  private(j,i) shared(a,n) default(none) 
-    //     for(j=0;j<n;j++)
-    //     {
-    //         if(j>=pow(2,i))
-    //         {
-    //             a[j] = a[(int)(j-pow(2,i-1))] + a[j];
-    //         }
-    //     }
-    // }
-
-
-    // int non_zero_dif = 0;
+    // printf("Exclusive scan performed serially: ");
     // for(i = 0;i<n;i++)
     // {
-    //     if( (int)(a[i] - a_ser[i]) != 0) non_zero_dif += 1;
+    //     cout << a_ser_excl[i] << " ";
     // }
-    // printf("Number of indices where serial and parallel sum have different values = %d\n",non_zero_dif);
+    // cout << endl;
+
+    // printf("Exclusive scan performed parallely using Blelloch Algorithm: ");
+    // for(i = 0;i<n;i++)
+    // {
+    //     cout << a3[i] << " ";
+    // }
+    // cout << endl;
+
+    //checking descrepancy for large a array a: 
+    // code used is same as above.
+
+
+    int non_zero_dif = 0;
+    for(i = 0;i<n;i++)
+    {
+        if( (int)(a[i] - a_ser_incl[i]) != 0) non_zero_dif += 1;
+    }    
+    printf("Number of indices where serial and parallel sum have different values for inclusive scan = %d\n",non_zero_dif);
     
-    // for(i = 0;i<n;i++)
-    // {
-    //     cout << a[i] << " ";
-    // }
-    // cout << endl;
-
-
-    // for(i = 0;i<n;i++)
-    // {
-    //     cout << a_ser[i] << " ";
-    // }
-    // cout << endl;
-
-
+    non_zero_dif = 0;
+    for(i = 0;i<n;i++)
+    {
+        if( (int)(a3[i] - a_ser_excl[i]) != 0) non_zero_dif += 1;
+    }
+    printf("Number of indices where serial and parallel sum have different values for exclusive scan = %d\n",non_zero_dif);
+    
     return 0;
 }
