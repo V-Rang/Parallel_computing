ef2b81d89d5bd9db828417fa8b56cf9684c85ca8
Venugopal Ranganathan <95169245+V-Rang@users.noreply.github.com>
2023-06-05 21:28:22 -0500

    designated master stores final sum

--- sum_example.cpp
+++ sum_example.cpp
@@ -1,42 +1,47 @@
 #include<iostream>
 #include<mpi.h>
+#include<iomanip>
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
     int my_node,total_nodes;
-    int sum,startval,endval,accum;
+    int startval,endval;
+    double sum, accum;
+    int master = 1;
     MPI_Status status;
     MPI_Init(&argc,&argv);
 
     MPI_Comm_rank(MPI_COMM_WORLD,&my_node);
     MPI_Comm_size(MPI_COMM_WORLD,&total_nodes);
 
-    sum = 0;
+    sum = 0.0;
     startval = my_node*1000/total_nodes + 1;
     endval = (my_node + 1)*1000/total_nodes;
 
     for(int i=startval;i<=endval;i++)
     {
-        sum = sum + i;
+        sum += (double)1/i;
     }
 
-    if(my_node != 0)
+    if(my_node != master)
     {
-        MPI_Send(&sum,1,MPI_INT,0,1,MPI_COMM_WORLD);
+        MPI_Send(&sum,1,MPI_DOUBLE,master,1,MPI_COMM_WORLD);
     }
     else
     {
-        for(int j=1;j<total_nodes;j++)
+        for(int j=0;j<total_nodes;j++)
         {
-            MPI_Recv(&accum,1,MPI_INT,j,1,MPI_COMM_WORLD,&status);
+            if(j == master) continue;
+            MPI_Recv(&accum,1,MPI_DOUBLE,j,1,MPI_COMM_WORLD,&status);
             sum = sum + accum;
         }
     }
 
-    if(my_node == 0)
+    if(my_node == master)
     {
+        cout << setprecision(20);
         cout << "The sum from 1 to 1000 is: " << sum << endl;
     }
 
