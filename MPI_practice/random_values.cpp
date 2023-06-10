b163ec9765c21458e49e878d37c8b219291f6dc9
Venugopal Ranganathan <95169245+V-Rang@users.noreply.github.com>
2023-06-10 18:15:01 -0500

    Example of changing values of x,y and z on 3 procs depending on the order of execution of different statements.

--- MPI_practice/random_values.cpp
+++ MPI_practice/random_values.cpp
@@ -16,7 +16,6 @@ int main(int argc, char **argv)
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&my_node);
     MPI_Comm_size(MPI_COMM_WORLD,&total_nodes);
-    MPI_Bcast()
     int x,y,z;
     switch(my_node)
     {
