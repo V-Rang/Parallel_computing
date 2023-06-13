c3802f305e665b0dee76a15621958a735a5c082f
Venugopal Ranganathan <95169245+V-Rang@users.noreply.github.com>
2023-06-13 16:58:50 -0500

    Update custom_data_structure.cpp

--- MPI_practice/custom_data_structure.cpp
+++ MPI_practice/custom_data_structure.cpp
@@ -7,68 +7,67 @@ custom data structure to communicate multiple variable values in MPI using MPI_T
 
 using namespace std;
 
-
-void Build_derived_datatype(float *a_ptr, float *b_ptr, int *n_ptr, MPI_Datatype *mesg_mpi)
+void build_cust_d(int *a, char *b, float *c, MPI_Datatype* mpi_mesg)
 {
-    int block_lengths[3];
-    block_lengths[0] = block_lengths[1] = block_lengths[2] = 1;
-
-    MPI_Datatype typelists[3];
-    typelists[0] = MPI_FLOAT;
-    typelists[1] = MPI_FLOAT;
-    typelists[2] = MPI_INT;
-
+    int blocklengths[3];
     MPI_Aint displacements[3];
-    displacements[0] = 0;
+    MPI_Datatype typelists[3];
+
+    blocklengths[0] = 3;
+    blocklengths[1] = 2;
+    blocklengths[2] = 4;
+
+    typelists[0] = MPI_INT;
+    typelists[1] = MPI_CHAR;
+    typelists[2] = MPI_FLOAT;
+
     MPI_Aint address;
+    MPI_Aint start_address;
 
-    MPI_Get_address(a_ptr,&address);
-    MPI_Aint start_address = address;
+    displacements[0] = 0;
 
+    MPI_Get_address(a,&start_address);
+    MPI_Get_address(b,&address);
 
-    MPI_Get_address(b_ptr,&address);
     displacements[1] = address - start_address;
 
+    MPI_Get_address(c,&address);
 
-    MPI_Get_address(n_ptr,&address);
     displacements[2] = address - start_address;
 
-
-    MPI_Type_create_struct(3,block_lengths,displacements,typelists,mesg_mpi);
-
-    MPI_Type_commit(mesg_mpi);
-
+    MPI_Type_create_struct(3,blocklengths,displacements,typelists,mpi_mesg);
+    MPI_Type_commit(mpi_mesg);
+    
 }
-
 
 int main(int argc, char **argv)
 {
     int my_node,total_nodes;
-    float *a_ptr,*b_ptr;
-    int *n_ptr;
-    
-    a_ptr = new float[1];
-    b_ptr = new float[1];
-    n_ptr = new int[1];
-    MPI_Status status;
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&my_node);
     MPI_Comm_size(MPI_COMM_WORLD,&total_nodes);
+    int *a = new int[3]{1,2,3};
+    char *b = new char[2]{'a','b'};
+    float *c = new float[4]{1.2,2.3,3.4,4.5};
 
     if(my_node == 0)
     {
-        printf("Enter the values of a, b and n\n");
-        fflush(stdout);
-        scanf("%f %f %d",a_ptr,b_ptr,n_ptr);
+        b[1] = 'c';
+        c[3] = 5.6;
     }
 
-    MPI_Datatype mpi_mesg;
+    MPI_Datatype new_mpi_t;
+    
+    build_cust_d(a,b,c,&new_mpi_t);
+    MPI_Bcast(a,1,new_mpi_t,0,MPI_COMM_WORLD);
 
-    Build_derived_datatype(a_ptr,b_ptr,n_ptr,&mpi_mesg);
 
-    MPI_Bcast(a_ptr,1,mpi_mesg,0,MPI_COMM_WORLD);
+    if(my_node == 1)
+    {
+        cout << b[1] << " " << c[3] << endl;
+    }
 
-    printf("Process %d has the values of a, b and n = %f, %f, %d\n",my_node,*a_ptr,*b_ptr,*n_ptr);
 
     MPI_Finalize();
+
 }
