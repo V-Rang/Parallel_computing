1df6bfd8ed4cbc477f106502985816d94b00ef0e
Venugopal Ranganathan <95169245+V-Rang@users.noreply.github.com>
2023-06-24 13:05:23 -0500

    Create initial.cu

---
+++ Cuda_Practice/initial.cu
@@ -0,0 +1,21 @@
+#include<cuda.h>
+#include<cuda_runtime.h>
+#include<iostream>
+
+using namespace std;
+
+__global__ void check()
+{
+
+	int tid = blockIdx.x * blockDim.x + threadIdx.x;
+	printf("Hello from thread %d\n",tid);
+}
+
+int main()
+{
+	int num_blocks = 2;
+	int num_threads = 8;
+	check<<<num_blocks,num_threads>>>();	
+
+	return 0;
+}
