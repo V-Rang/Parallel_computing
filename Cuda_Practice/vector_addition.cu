55c8ca4b23265dd8c6ff183d38907c599d764be1
Venugopal Ranganathan <95169245+V-Rang@users.noreply.github.com>
2023-06-24 12:42:14 -0500

    Update vector_addition.cu

--- Cuda_Practice/vector_addition.cu
+++ Cuda_Practice/vector_addition.cu
@@ -1,45 +1,77 @@
 #include<cuda.h>
 #include<cuda_runtime.h>
 #include<iostream>
+#include<assert.h>
 
 using namespace std;
 
-__global__ void vectorAdd(float *x, float *y, float *z, int n)
+__global__ void vectorAdd(float *a, float *b, float*c, int n)
 {
-	int tid = blockDim.x * blockIdx.x + threadIdx.x;
-	if(tid < n) z[tid] = x[tid] + y[tid];
+	int tid = blockIdx.x * blockDim.x + threadIdx.x;
+	if(tid < n) c[tid] = a[tid] + b[tid];
+}
+
+
+void verify_result(float *&a, float *&b, float *c, int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		// printf("iteration = %d\n",i);
+		assert(c[i] == a[i] + b[i] );
+	}
 }
 
 int main()
 {
-	float *a, *b, *c;
-	float *d_a,*d_b,*d_c;
+	int n = 1 << 16;
+	// int n = 10;
+	size_t size = n * sizeof(float);
 
-	int n = 5;
+	float *h_x,*h_y,*h_z;
 
-	a = new float[5]{1,2,3,4,5};
-	b = new float[5]{11,12,13,14,15};
+	float *d_x,*d_y,*d_z;
 
-	c = new float[5];
+	h_x = new float[n];
+	h_y = new float[n];
+	h_z = new float[n];
 
-	int size = n * sizeof(float);
+	cudaMalloc(&d_x,size);
+	cudaMalloc(&d_y,size);
+	cudaMalloc(&d_z,size);
 
-	cudaMalloc(&d_a,size);
-	cudaMalloc(&d_b,size);
-	cudaMalloc(&d_c,size);
+	for(int i=0;i<n;i++)
+	{
+		h_x[i] = rand()%100;
+		h_y[i] = rand()%100; 
+	}
 
-	cudaMemcpy(d_a,a,size,cudaMemcpyHostToDevice);
-	cudaMemcpy(d_b,b,size,cudaMemcpyHostToDevice);
-	cudaMemcpy(d_c,c,size,cudaMemcpyHostToDevice);
+
+	cudaMemcpy(d_x,h_x,size,cudaMemcpyHostToDevice);
+	cudaMemcpy(d_y,h_y,size,cudaMemcpyHostToDevice);
+	cudaMemcpy(d_z,h_z,size,cudaMemcpyHostToDevice);
+
+	int num_threads = 256;
+	int num_blocks = n/num_threads;
+
+	vectorAdd<<<num_blocks,num_threads>>>(d_x,d_y,d_z,n);
+	// vectorAdd<<<1,1024>>>(d_x,d_y,d_z,n); //this gives error with 1 << 16 elements.
+
+	cudaMemcpy(h_z,d_z,size,cudaMemcpyDeviceToHost);
+
+	// for(int i=0;i<n;i++)
+	// {
+	// 	cout << h_x[i] << " " << h_y[i] << " " << h_z[i] << endl;
+	// }
+
+	verify_result(h_x,h_y,h_z,n);
 	
-	vectorAdd<<<1,1024>>>(d_a,d_b,d_c,n);
-
-	cudaMemcpy(c,d_c,size,cudaMemcpyDeviceToHost);
-
-	for(int i=0;i<n;i++) std::cout << c[i] << " ";
-	std::cout << endl;
 	
-
+	cudaFree(d_x);
+	cudaFree(d_y);
+	cudaFree(d_z);
 
 	return 0;
 }
+
+
+
