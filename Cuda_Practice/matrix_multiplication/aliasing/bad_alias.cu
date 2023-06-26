7530bd51bf62f0fb363fd6126ab61ffdc17126d2
Venugopal Ranganathan <95169245+V-Rang@users.noreply.github.com>
2023-06-26 12:29:21 -0500

    Update bad_alias.cu

--- Cuda_Practice/matrix_multiplication/aliasing/bad_alias.cu
+++ Cuda_Practice/matrix_multiplication/aliasing/bad_alias.cu
@@ -2,6 +2,7 @@
 (transposing B and then traversing the elements of its rows, i.e. each thread accesses a unique row of Bt, each of which are displaced far 
 from each other in memory. This results in bad memory usage as data for threads cannot be found with single call to DRAM that will load the 
 values needed by the threads in the cache.)
+timing using NsightCompute: 75.92 ms
 */
 
 #include<iostream>
