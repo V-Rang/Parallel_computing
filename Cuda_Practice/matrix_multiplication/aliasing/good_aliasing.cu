cbc63b568dca8094ae206d72c6a6b869bd995f91
Venugopal Ranganathan <95169245+V-Rang@users.noreply.github.com>
2023-06-26 12:20:58 -0500

    Update good_aliasing.cu

--- Cuda_Practice/matrix_multiplication/aliasing/good_aliasing.cu
+++ Cuda_Practice/matrix_multiplication/aliasing/good_aliasing.cu
@@ -15,40 +15,40 @@ Time: using NsightCompute: 87.56 ms
 
 using namespace std;
 
-__global__ void matrix_mult(int *a, int *b, int *c, int n)
+__global__ void matrix_mult(int* a, int* b, int* c, int n)
 {
-    int row = blockIdx.y*blockDim.y + threadIdx.y;
-    int col = blockIdx.x*blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-    c[row*n + col] = 0;
+    c[row * n + col] = 0;
 
-    for(int i=0;i<n;i++) c[row*n+col] += a[row*n + i]*b[i*n + col];
+    for (int i = 0; i < n; i++) c[row * n + col] += a[i * n + row] * b[i * n + col];
 
 }
 
-void verify_result(vector<int>&a, vector<int>&b, vector<int>&c, int n)
+void verify_result(vector<int>& a, vector<int>& b, vector<int>& c, int n)
 {
-    for(int i=0;i<n;i++) //for each row
+    for (int i = 0; i < n; i++) //for each row
     {
-        for(int j=0;j<n;j++) //for each col
+        for (int j = 0; j < n; j++) //for each col
         {
             int temp = 0;
-            for(int k=0;k<n;k++)
+            for (int k = 0; k < n; k++)
             {
-                temp += a[i*n + k]*b[k*n + j];
+                temp += a[i * n + k] * b[k * n + j];
             }
-            assert(temp == c[i*n+j]);
+            assert(temp == c[i * n + j]);
         }
     }
 }
 
-void transposer(vector<int>&a, vector<int>&a_t,int n)
+void transposer(vector<int>& a, vector<int>& a_t, int n)
 {
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
         {
-            a_t[j*n+i] = a[i*n+j];
+            a_t[j * n + i] = a[i * n + j];
         }
     }
 }
@@ -59,44 +59,44 @@ int main()
     int num_threads = 32;
     // int n = 4;
     // int num_threads = 2;
-    int num_blocks = n/num_threads;
+    int num_blocks = n / num_threads;
     size_t size = n * n * sizeof(int);
 
-    dim3 threads(num_threads,num_threads);
-    dim3 blocks(num_blocks,num_blocks);
+    dim3 threads(num_threads, num_threads);
+    dim3 blocks(num_blocks, num_blocks);
 
-    vector<int>h_a(n*n), h_b(n*n), h_c(n*n),h_a_t(n*n);
+    vector<int>h_a(n * n), h_b(n * n), h_c(n * n);
 
-    int *d_a, *d_b, *d_c;
+    vector<int>h_a_t(n * n);
 
-    cudaMalloc(&d_a,size);
-    cudaMalloc(&d_b,size);
-    cudaMalloc(&d_c,size);  
+    int* d_a, * d_b, * d_c;
 
-    generate(h_a.begin(), h_a.end(), [](){return rand()%100;});
-    generate(h_b.begin(), h_b.end(), [](){return rand()%100;});
+    cudaMalloc(&d_a, size);
+    cudaMalloc(&d_b, size);
+    cudaMalloc(&d_c, size);
 
-    transposer(h_a,h_a_t,n);
+    generate(h_a.begin(), h_a.end(), []() {return rand() % 100; });
+    generate(h_b.begin(), h_b.end(), []() {return rand() % 100; });
 
-    
-
-    cudaMemcpy(d_a,h_a.data(),size,cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b,h_b.data(),size,cudaMemcpyHostToDevice);
-    
-
-    matrix_mult<<<blocks,threads>>>(d_a,d_b,d_c,n);
-
-    cudaMemcpy(h_c.data(),d_c,size,cudaMemcpyDeviceToHost);
+    transposer(h_a, h_a_t, n);
 
 
-    verify_result(h_a,h_b,h_c,n);
+    cudaMemcpy(d_a, h_a_t.data(), size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b.data(), size, cudaMemcpyHostToDevice);
+
+    matrix_mult << <blocks, threads >> > (d_a, d_b, d_c, n);
+
+    cudaMemcpy(h_c.data(), d_c, size, cudaMemcpyDeviceToHost);
+
+    verify_result(h_a, h_b, h_c, n);
 
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
-    
+
 
     printf("Successful\n");
 
     return 0;
 }
+
